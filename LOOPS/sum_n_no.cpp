b3a6a3dd518880842eeb7465feb7366d5d7effef
ParthbBhut <parthbbhut1622@gmail.com>
2022-03-10 16:08:51 +0530

    Create sum_n_no.cpp

---
+++ LOOPS/sum_n_no.cpp
@@ -0,0 +1,18 @@
+// Sum  of a N Number 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,sum = 0;
+    cin >> n;
+    for(int i=1;i<=n;i++)
+    {
+        sum = sum + i;
+        cout << sum << " " ;
+    }
+    
+    
+
+}
