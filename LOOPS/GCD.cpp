cd165ec42ff59fda41fb6326638c3e499db10acc
ParthbBhut <parthbbhut1622@gmail.com>
2022-03-10 16:24:35 +0530

    Create GCD.cpp

---
+++ LOOPS/GCD.cpp
@@ -0,0 +1,22 @@
+// GCD  
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int m,n ;
+   cin >> m >> n;
+   while(m!=n)
+   {
+       if(m>n)
+          m -= n;
+       else if (n>m)
+          n -= m;
+   }
+   cout << m;
+    
+    
+    return 0;
+
+}
